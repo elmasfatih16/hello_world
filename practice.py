71089d0d43002fb4e7efd74379659f0600370888
Fatih <75074321+elmasfatih16@users.noreply.github.com>
2021-02-17 21:41:14 +0300

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+
+"""x=10
+y=20
+print("x= " + str(y))
+print("y= " + str(x))"""
+
+"""temp=x
+x=y
+y=temp
+"""
+# x,y=y,x
+# a=input("mil cinsinsinden uzunluk girin: ")
+# b=float(a)*1.609344
+# print("girdiğiniz mil cinsinden uzunluğa karşılık gelen km değeri: " + str(b))
+
+# bff=["nesli","derya","iea","eren"]
+# print(bff)
+# print(bff[2])
+# bff.append("acd")
+# bff.remove("nesli")
+# print(bff)
+# print(bff[2])
+
+# fav_song=list(("dancing queen","love of my life", "mama said"))
+# print(fav_song)
+# print("the number of my favorite songs is " + str(len(fav_song)))
+# fav_song.reverse()
+# print(fav_song)
+# fav_song.pop(1)
+# fav_song.insert(0,"vogue")
+# fav_song.reverse()
+# print(fav_song)
+# fav_song.append("english man in new york")
+# print(fav_song)
+# fav_song[1]="still the one"
+# print(fav_song)
+# fav_song2= fav_song.copy()
+# fav_song.append("nothing else matter")
+# print(fav_song)
+# print(fav_song2)
+
+# fav_painter=list(("monet","matisse","rembrandt"))
+# print(fav_painter)
+# fav_director=list(("kurosawa","bergman","tarkovski"))
+# print(fav_director)
+# fav_architecture=list(("bramante","sinan","le corbusier","van der rohe","renzo piano"))
+# print(fav_architecture)
+# fav_artist=["aliye berger","yıldız moran","füreya koral"]
+# print(fav_artist)
+# fav_artist.reverse()
+# print(fav_artist)
+# fav_artist.extend(fav_architecture + fav_director + fav_painter)
+# fav_artist.sort()
+# print(fav_artist)
+
+# a=(17,27,57,"taşkışla",(0,2,14),[])
+# b=[17,27,57,"taşkışla",(0,2,14),()]
+# print(a)
+# print(b)
+# print(type(a))
+# print(type(b))
+# print(int(len(b)) + int(len(a)))
+# print(a[-1]) 
+# print(b[-2])
+# c=("A107",)
+# print(type(c))
+
+# student={"ismail","engin","aksoy"}
+# print(type(student))
+# print(student)
+# for i in student:
+#     print(i)
+#     print("engin"in student)
+#     print("Engin"in student)
+#     if "engin"in student :
+#         print("there is in set")
+#     else:
+#         print("there is not in set")
+# student.add("efe")
+# print(student)
+# student.update(["eren","ahmet"])
+# print(student)
+# student.remove("aksoy")
+# student.discard("ismail")
+# print(student)
+
+# dor_mate=student
+# print(dor_mate)
+# dor_mate.clear()
+# print(dor_mate)
+
+# set_a={1,2,3,4,5,6,6}
+# set_b={10,9,8,7,6,5,4}
+# set_c={0,1}
+# print(set_a | set_b)
+# print(set_a.union(set_c))
+
+# print(set_a & set_b)
+# print(set_a.intersection(set_c))
+
+# print(set_a - set_b)
+# print(set_a.difference(set_c))
+
+# print(set_a ^ set_b)
+# print(set_a.symmetric_difference(set_c))
+
+# de_sözlük={
+#         "eine":"bir", 
+#         "zwei":"iki", 
+#         "drei":"üç", 
+#         "vier":"dört"
+#         }
+# print(de_sözlük)
+# print(de_sözlük["zwei"])
+# de_sözlük["fünf"]="beş"
+# print(de_sözlük)
+# tr_sözlük=dict(bir="eine", iki="zwei", üç="drei", dört="vier", beş="fünf")
+# print(tr_sözlük)
+
+
+
+
+
+
+
+
