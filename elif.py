2783e7f71946b22f2fc9537b5a12d20e7bc7125f
Fatih <75074321+elmasfatih16@users.noreply.github.com>
2021-02-21 02:36:13 +0300

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,40 @@
+# num_1=10
+# num_2=100
+# a=input('please give a number: ')
+# if int(a)<num_1:
+#     print('your number has got one digit.')
+# elif num_1<=int(a)<num_2:
+#     print('your number has got two digit.')
+# else:
+#     print('your number has got more than two digit.')
+
+# lights=["red","yellow","green"]
+# current_light=lights[1]
+# print(current_light)
+# if current_light=="red":
+#     print("you must stop")
+# elif current_light=="yellow":
+#     print("you should be ready")
+# else:
+#     print("you must go on")
+
+# a=float(input("please select a number: "))
+# print(a)
+# if a>0:
+#     print("your number is possitive")
+# elif a<0:
+#     print("your number is negative")
+# else:
+#     print("your number is zero")
+
+# x=int(input("please give a number: "))
+# y=int(input("please give a number: "))
+# z=int(input("please give a number: "))
+# if x>y>z or x>z>y:
+#     print(str(x)+" is the greatest given number")
+# elif y>x>z or y>z>x:
+#     print(str(y)+" is the greatest given number")
+# else:
+#     print(str(z)+" is the greatest given number")
+
+
